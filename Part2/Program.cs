c9666a534b2fd87ab732827683254bb9ca130e0b
Cielo Vazquez <W7296315@student.miracosta.edu>
2021-08-24 23:24:43 -0700

    Add project files.

---
+++ Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: MyFirstProgram.java
+/// @author: Cielo
+/// Date:  August 24, 2021
+///
+/// Problem Statement: Write a program that displays a simple picture of your choice. 
+/// It could be a car, a house, a flower, a tree, a square, a 3-D box, a star or other shapes
+/// 
+/// 
+/// Overall Plan:
+/// 1) type out design 
+/// 2) Test the design
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___________");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|__________|");
+            //just pause screen to be able to read output
+            Console.ReadLine();
+
+        }
+    }
+}
